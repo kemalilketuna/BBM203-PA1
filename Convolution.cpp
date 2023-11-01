eab869c2cc318c5344051ba0293bdf8559943b69
Kemal İlke Tuna <35730029+kemalilketuna@users.noreply.github.com>
2023-11-01 18:26:22 +0300

    Convolution done 2

--- Convolution.cpp
+++ Convolution.cpp
@@ -19,22 +19,19 @@ Convolution::Convolution(double** customKernel, int kh, int kw, int stride_val, 
 
 // Destructor
 Convolution::~Convolution() {
-        for (int i = 0; i < kernelHeight; ++i) {
-            delete[] kernel[i];
-        }
-        delete[] kernel;
+    for (int i = 0; i < kernelHeight; ++i) {
+        delete[] kernel[i];
+    }
+    delete[] kernel;
 }
 
 // Copy constructor
-Convolution::Convolution(const Convolution &other){
-    this->kernelHeight = other.kernelHeight;
-    this->kernelWidth = other.kernelWidth;
-    this->stride = other.stride;
-    this->padding = other.padding;
-    kernel = new double*[other.kernelHeight];
-    for (int i = 0; i < other.kernelHeight; ++i) {
-        kernel[i] = new double[other.kernelWidth];
-        for (int j = 0; j < other.kernelWidth; j++) {
+Convolution::Convolution(const Convolution &other)
+    : kernelHeight(other.kernelHeight), kernelWidth(other.kernelWidth), stride(other.stride), padding(other.padding) {
+    kernel = new double*[kernelHeight];
+    for (int i = 0; i < kernelHeight; ++i) {
+        kernel[i] = new double[kernelWidth];
+        for (int j = 0; j < kernelWidth; ++j) {
             kernel[i][j] = other.kernel[i][j];
         }
     }
@@ -42,23 +39,23 @@ Convolution::Convolution(const Convolution &other){
 
 // Copy assignment operator
 Convolution& Convolution::operator=(const Convolution &other) {
-    if (this == &other) {
-        return *this;
-    }
-    // Free the existing memory
-    for (int i = 0; i < kernelHeight; ++i) {
-        delete[] kernel[i];
-    }
-    delete[] kernel;
-    this->kernelHeight = other.kernelHeight;
-    this->kernelWidth = other.kernelWidth;
-    this->stride = other.stride;
-    this->padding = other.padding;
-    kernel = new double*[other.kernelHeight];
-    for (int i = 0; i < other.kernelHeight; ++i) {
-        kernel[i] = new double[other.kernelWidth];
-        for (int j = 0; j < other.kernelWidth; j++) {
-            kernel[i][j] = other.kernel[i][j];
+    if (this != &other) {
+        for (int i = 0; i < kernelHeight; ++i) {
+            delete[] kernel[i];
+        }
+        delete[] kernel;
+
+        kernelHeight = other.kernelHeight;
+        kernelWidth = other.kernelWidth;
+        stride = other.stride;
+        padding = other.padding;
+
+        kernel = new double*[kernelHeight];
+        for (int i = 0; i < kernelHeight; ++i) {
+            kernel[i] = new double[kernelWidth];
+            for (int j = 0; j < kernelWidth; ++j) {
+                kernel[i][j] = other.kernel[i][j];
+            }
         }
     }
     return *this;
@@ -67,12 +64,9 @@ Convolution& Convolution::operator=(const Convolution &other) {
 
 // Convolve Function: Responsible for convolving the input image with a kernel and return the convolved image.
 ImageMatrix Convolution::convolve(const ImageMatrix& input_image) const {
-    int paddingValue = padding ? 1 : 0;
-    int newImage_height = (input_image.get_height() + 2 * paddingValue);
-    int newImage_width = (input_image.get_width() + 2 * paddingValue);
-    ImageMatrix newImage = ImageMatrix(newImage_height, newImage_width);
-    if (padding) {
-        // Surround top bottom left right with zeros
+    ImageMatrix newImage;
+    if (padding){
+        newImage = ImageMatrix(input_image.get_height() + 2, input_image.get_width() + 2);
         for (int i = 0; i < newImage.get_height(); ++i) {
             for (int j = 0; j < newImage.get_width(); ++j) {
                 if (i == 0 || j == 0 || i == newImage.get_height() - 1 || j == newImage.get_width() - 1) {
@@ -83,18 +77,16 @@ ImageMatrix Convolution::convolve(const ImageMatrix& input_image) const {
             }
         }
     } else {
-        for (int i = 0; i < newImage.get_height(); ++i) {
-            for (int j = 0; j < newImage.get_width(); ++j) {
-                newImage.get_data()[i][j] = input_image.get_data()[i][j];
-            }
-        }
+        newImage = ImageMatrix(input_image.get_height(), input_image.get_width());
+        newImage = input_image;
     }
 
-    int convolvedImage_height = (input_image.get_height() - kernelHeight + 2 * paddingValue) / stride + 1;
-    int convolvedImage_width = (input_image.get_width() - kernelWidth + 2 * paddingValue) / stride + 1;
+
+    int convolvedImage_height = (input_image.get_height() - kernelHeight + 2 * padding) / stride + 1;
+    int convolvedImage_width = (input_image.get_width() - kernelWidth + 2 * padding) / stride + 1;
     ImageMatrix convolvedImage(convolvedImage_height, convolvedImage_width);
-    for (int i = 0; i <= newImage_height - kernelHeight; i += stride) {
-        for (int j = 0; j <= newImage_width - kernelWidth; j += stride) {
+    for (int i = 0; i <= newImage.get_height() - kernelHeight; i += stride) {
+        for (int j = 0; j <= newImage.get_width() - kernelWidth; j += stride) {
             double sum = 0;
             for (int k = 0; k < kernelHeight; ++k) {
                 for (int l = 0; l < kernelWidth; ++l) {
