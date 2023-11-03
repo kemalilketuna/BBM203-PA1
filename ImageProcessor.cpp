5a5abcf4db156b8ac4f75bdf4e30acf518ab0970
Kemal İlke Tuna <35730029+kemalilketuna@users.noreply.github.com>
2023-11-03 21:09:42 +0300

    last

--- ImageProcessor.cpp
+++ ImageProcessor.cpp
@@ -19,15 +19,27 @@ std::string ImageProcessor::decodeHiddenMessage(const ImageMatrix &img) {
 
     // Create an EdgeDetector object
     EdgeDetector edgeDetector;
+    std::vector<std::pair<int, int>> edgePixels = edgeDetector.detectEdges(sharpenedImg);
 
+    DecodeMessage decodeMessage;
     // Create a DecodeMessage object and decode the hidden message from the sharpened image
-    return DecodeMessage().decodeFromImage(sharpenedImg, edgeDetector.detectEdges(sharpenedImg));
+    return decodeMessage.decodeFromImage(sharpenedImg, edgePixels);
 }
 
 ImageMatrix ImageProcessor::encodeHiddenMessage(const ImageMatrix &img, const std::string &message) {
+    // Create an ImageSharpening object
+    ImageSharpening imgSharpener;
+
+    // Sharpen the image
+    ImageMatrix sharpenedImg = imgSharpener.sharpen(img, 2);
+    
     // Create an EdgeDetector object
     EdgeDetector edgeDetector;
+    std::vector<std::pair<int, int>> edgePixels = edgeDetector.detectEdges(sharpenedImg);
+
+    // Create an EncodeMessage object
+    EncodeMessage encodeMessage;
 
     // Create an EncodeMessage object and encode the message to the image
-    return EncodeMessage().encodeMessageToImage(img, message, edgeDetector.detectEdges(ImageSharpening().sharpen(img, 2)));
+    return encodeMessage.encodeMessageToImage(img, message, edgePixels);
 }
